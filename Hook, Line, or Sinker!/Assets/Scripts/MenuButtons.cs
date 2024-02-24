276be028bfc7e04983d04b8725c62a8f8c3d2bbc
bbt795 <bubby1031@hotmail.com>
2024-02-23 21:24:40 -0500

    Implement Basic Main Menu

---
+++ Hook, Line, or Sinker!/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void NewGame()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void ExitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+}
