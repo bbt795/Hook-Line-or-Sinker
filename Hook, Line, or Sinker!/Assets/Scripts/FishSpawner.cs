0e9435fac388879e083bfe6691c8c7e28087a065
nyaspurrexe <aookami83@gmail.com>
2024-02-24 17:46:07 -0500

    fish are now randomized when spawning in

--- Hook, Line, or Sinker!/Assets/Scripts/FishSpawner.cs
+++ Hook, Line, or Sinker!/Assets/Scripts/FishSpawner.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
 {
-    public Collider2D polygonCollider;
+    public Collider2D boxCollider;
     public int numberRandomPositions = 10;
 
-    public GameObject prefab;
+    public List<GameObject> prefabPool;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (polygonCollider == null) GetComponent<Collider2D>();
-        if (polygonCollider == null) Debug.Log("Please assign Collider2D component.");
+        if (boxCollider == null)
+        {
+            GetComponent<Collider2D>();
+        }
+        if (boxCollider == null)
+        {
+            Debug.Log("Please assign Collider2D component.");
+        }
+
+        
+        //int prefabSpawned = Random.Range(0, prefabPool.Count);
+        //GameObject toSpawn = prefabPool[prefabSpawned];
 
         int i = 0;
         while (i < numberRandomPositions)
         {
-            Vector3 rndPoint3D = RandomPointInBounds(polygonCollider.bounds, 1f);
+            Vector3 rndPoint3D = RandomPointInBounds(boxCollider.bounds, 1f);
             Vector2 rndPoint2D = new Vector2(rndPoint3D.x, rndPoint3D.y);
-            Vector2 rndPointInside = polygonCollider.ClosestPoint(new Vector2(rndPoint2D.x, rndPoint2D.y));
+            Vector2 rndPointInside = boxCollider.ClosestPoint(new Vector2(rndPoint2D.x, rndPoint2D.y));
+
             if (rndPointInside.x == rndPoint2D.x && rndPointInside.y == rndPoint2D.y)
             {
-                GameObject rndCube = Instantiate(prefab, rndPointInside, Quaternion.identity);
+                GameObject rndCube = Instantiate(prefabPool[Random.Range(0, prefabPool.Count)], rndPointInside, Quaternion.identity);
                 rndCube.transform.position = rndPoint2D;
                 i++;
             }
