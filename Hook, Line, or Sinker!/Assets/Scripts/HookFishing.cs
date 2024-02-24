89b9776f9fc6ebad235000bdb364b8ddfdd58036
BananaCat876 <jacq.custer@gmail.com>
2024-02-24 17:03:55 -0500

    Altered Hook Prefab
    Edited code
    Removed gravity

--- Hook, Line, or Sinker!/Assets/Scripts/HookFishing.cs
+++ Hook, Line, or Sinker!/Assets/Scripts/HookFishing.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 
 public class HookFishing : MonoBehaviour
 {
+    public Animator myAnim;
+    public SpriteRenderer myRenderer;
+    public Rigidbody2D myRig;
     // Start is called before the first frame update
     void Start()
     {
+        myAnim = this.GetComponent<Animator>();
+        myRenderer = this.GetComponent<SpriteRenderer>();
+        myRig = this.GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        myRig.velocity = new Vector2(0, -3);
         
     }
 }
