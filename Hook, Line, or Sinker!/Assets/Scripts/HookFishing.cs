f9c27a1303c9ad0d75e6d207702ffafea091c4f0
BananaCat876 <jacq.custer@gmail.com>
2024-02-24 17:39:28 -0500

    Hook Reverses Direction

--- Hook, Line, or Sinker!/Assets/Scripts/HookFishing.cs
+++ Hook, Line, or Sinker!/Assets/Scripts/HookFishing.cs
@@ -8,6 +8,10 @@ public class HookFishing : MonoBehaviour
     public Animator myAnim;
     public SpriteRenderer myRenderer;
     public Rigidbody2D myRig;
+    public float speed = 2f;
+    public bool isReversing = false;
+    public float stopDistance = 0.5f; // Adjust as needed
+    public float targetYPosition = -38.5f; 
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +24,20 @@ public class HookFishing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myRig.velocity = new Vector2(0, -3);
+        //Slow down/Reverse direction at -38.5 y
+       //Vector3 movement = Vector3.down * speed * Time.deltaTime;
+
+        // Move the object
+        //myRig.transform.Translate(movement);
+
+        myRig.velocity = Vector2.down * speed;
+        if (!isReversing && transform.position.y <= targetYPosition)
+        {
+            isReversing = true;
+            // Reverse the direction
+            speed *= -1f;
+            //myRig.velocity *= -1f;
+        }
         
     }
 }
