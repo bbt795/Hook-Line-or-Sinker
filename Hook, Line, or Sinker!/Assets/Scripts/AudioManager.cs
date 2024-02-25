baaf43c02647aefff8c271306427e675db3d231d
nyaspurrexe <aookami83@gmail.com>
2024-02-25 02:57:51 -0500

    added music assets

--- Hook, Line, or Sinker!/Assets/Scripts/AudioManager.cs
+++ Hook, Line, or Sinker!/Assets/Scripts/AudioManager.cs
@@ -7,7 +7,13 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // Update is called once per frame
