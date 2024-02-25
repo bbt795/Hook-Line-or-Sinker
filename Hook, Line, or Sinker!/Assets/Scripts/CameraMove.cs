391046c0d7db610e99e99498ea714b6a5f17de86
BananaCat876 <jacq.custer@gmail.com>
2024-02-24 23:37:39 -0500

    Added scene change to Minigame

--- Hook, Line, or Sinker!/Assets/Scripts/CameraMove.cs
+++ Hook, Line, or Sinker!/Assets/Scripts/CameraMove.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class CameraMove : MonoBehaviour
 {
+    public string tinderScene;
     public float speed = 3f;
     public bool isReversing = false;
     public float targetYPosition = -38.5f; 
+    public float changeYPosition = 4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +28,10 @@ public class CameraMove : MonoBehaviour
             isReversing = true;
             // Reverse the direction
             speed *= -5f;
+        }
+        if(isReversing && transform.position.y == changeYPosition)
+        {
+            SceneManager.LoadScene(tinderScene);
         }
     }
 }
