d87374c80098c46c5d696ffbde37c143a9c9851c
nyaspurrexe <aookami83@gmail.com>
2024-02-24 19:29:10 -0500

    fish can swim back and forth in the water

---
+++ Hook, Line, or Sinker!/Assets/Scripts/FishMove.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour
+{
+    public Transform thisTransform;
+    public float moveSpeed = 0.2f;
+
+    public Vector2 decisionTime = new Vector2(1, 4);
+    public float decisionTimeCount = 0;
+
+    public Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.left, Vector3.forward, Vector3.back, Vector3.zero, Vector3.zero };
+    public int currentMoveDirection;
+
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = this.transform;
+        decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+
+        ChooseMoveDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisTransform.position += moveDirections[currentMoveDirection] * Time.deltaTime * moveSpeed;
+
+        if (decisionTimeCount > 0)
+        {
+            decisionTimeCount -= Time.deltaTime;
+
+            if (currentMoveDirection == 1)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if(currentMoveDirection == 2)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+
+            ChooseMoveDirection();
+        }
+    }
+
+    void ChooseMoveDirection()
+    {
+        currentMoveDirection = Mathf.FloorToInt(Random.Range(0, moveDirections.Length));
+    }
+}
